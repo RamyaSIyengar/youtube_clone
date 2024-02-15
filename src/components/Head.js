80cad0908700d6d0638aa80512ff6f07bba63bcf
RamyaSIyengar <ramyaiyengar12@gmail.com>
2024-02-16 03:55:32 +0530

    sidebar

--- src/components/Head.js
+++ src/components/Head.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Head = () => {
   return (
-    <div className='grid grid-flow-col p-4 m-2 shadow-lg' >
+    <div className='grid grid-flow-col p-3 m-2 shadow-lg' >
       <div className='flex col-span-1'>
         <img
         className='h-10 mx-4'
@@ -16,17 +16,29 @@ const Head = () => {
         />
       </div>
 
-      <div className='col-span-10' >
-        <input className="w-[50%] h-10 border border-gray-500 rounded-l-full" type='text' placeholder='  Search'></input>
-        <button className='border h-10 bg-gray-100 border-gray-500 rounded-r-full px-8 py-1'> 🔍</button>
+      <div className='col-span-11' >
+        <input className="w-[50%] h-10 border border-gray-500 rounded-l-full pl-10 ml-36" type='text' placeholder='  Search'></input>
+        <button className='border h-10 bg-gray-100 border-gray-500 rounded-r-full px-6 py-1'> 🔍</button>
+        <button><img  alt="microphone" className='h-6  px-2 mx-2 ' src="https://static.vecteezy.com/system/resources/previews/013/658/851/original/mic-microphone-record-speak-clip-art-icon-vector.jpg" /></button>
       </div>
 
-      <div className='col-span-1'>
+      <div className='col-span-1 flex mx-2 my-1'>
+      <img 
+        className='h-8 '
+        alt="bellIcon"
+        src='https://static.thenounproject.com/png/3750242-200.png'
+        />
         <img 
-        className='h-10'
+        className='h-7 mx-4'
+        alt="bellIcon"
+        src='https://static.vecteezy.com/system/resources/thumbnails/002/359/770/small/bell-icon-free-vector.jpg'
+        />
+        <img 
+        className='h-8 '
         alt='user'
          src='https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png' 
          />
+
       </div>
 
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,51 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='p-4  shadow-lg w-52'>
+      <h1 className='m-3 cursor-pointer hover:bg-gray-100 rounded-lg'>Home</h1>
+      <h1 className='m-3 cursor-pointer hover:bg-gray-100 rounded-lg'>Shorts</h1>
+      <h1 className='m-3 cursor-pointer hover:bg-gray-100 rounded-lg'>Subscriptions</h1>
+      <div className="border-b border-gray-300 my-4"></div> 
+       
+      <h1 className='m-3 cursor-pointer font-bold'>You &nbsp;  {">"}</h1>
+      <ul className='m-3 cursor-pointer '>
+        <li className='pt-3 px-2 hover:bg-gray-100 rounded-lg'>Your channel</li>
+        <li className='pt-3 px-2 hover:bg-gray-100 rounded-lg'>History</li>
+        <li className='pt-3 px-2 hover:bg-gray-100 rounded-lg'>Your videos</li>
+        <li className='pt-3 px-2 hover:bg-gray-100 rounded-lg'>Watch later</li>
+        <li className='pt-3 px-2 hover:bg-gray-100 rounded-lg'>Your clips</li>
+        <li className='pt-3 px-2 hover:bg-gray-100 rounded-lg'>Liked videos</li>
+      </ul>
+       
+      <h1 className='m-3 cursor-pointer font-bold pt-5'>Explore  {">"}</h1>
+      <ul className='m-3 cursor-pointer '>
+        <li className='pt-3 px-2 hover:bg-gray-100 rounded-lg'>Movies</li>
+        <li className='pt-3 px-2 hover:bg-gray-100 rounded-lg'>Trending</li>
+        <li className='pt-3 px-2 hover:bg-gray-100 rounded-lg'>Shopping</li>
+        <li className='pt-3 px-2 hover:bg-gray-100 rounded-lg'>Music</li>
+        <li className='pt-3 px-2 hover:bg-gray-100 rounded-lg'>Live</li>
+        <li className='pt-3 px-2 hover:bg-gray-100 rounded-lg'>Gaming</li>
+      </ul>
+
+      
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+    </div>
+    
   )
 }
 
